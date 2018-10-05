1fe8f6f82aec5d1584c18aeeab6c66c4d97372a3
viratpasumarthi <vpasumarthi1@niu.edu>
2018-10-05 18:21:13 -0500

    first commit

---
+++ Sampleconsole2015/Sampleconsole2015/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleconsole2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2015");
+        }
+    }
+}
