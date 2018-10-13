87c0ca1194458dd586e996bd0b56f23e9ad5c4b2
viratpasumarthi <vpasumarthi1@niu.edu>
2018-10-12 22:46:08 -0500

    added Driver time out

--- Sampleconsole2015/TestProject/Class1.cs
+++ Sampleconsole2015/TestProject/Class1.cs
@@ -9,19 +9,22 @@ using System.Threading.Tasks;
 
 namespace TestProject
 {
-    public class Class1
+    public class Class1 :Baseclass
     {
-        IWebDriver driver;
+        //public object Driver { get; private set; }
+
+        //IWebDriver driver;
         [SetUp]
         public void initialise()
         {
             var timespan = TimeSpan.FromMinutes(3);
             var options = new FirefoxOptions();
+            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
            // var service = new FirefoxDriverService()
-            driver = new FirefoxDriver();
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
+            //driver = new IWebDriver();
+          //  driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
             //new FirefoxDriver(FirefoxBinary,FirefoxProfile,timespan);
-            driver.Manage().Window.Maximize();
+            //driver.Manage().Window.Maximize();
 
 
             //var driver = new FirefoxDriver(binary, profile, timeSpan);
@@ -31,15 +34,16 @@ namespace TestProject
         public void OpenAppTets()
         {
             // gecko
+            Driver.Navigate().GoToUrl("http://www.google.com");
 
-            driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
+            //driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
             // driver.
         }
 
         [TearDown]
         public void EndTest()
         {
-            driver.Close();
+            Driver.Close();
         }
     }
 }
