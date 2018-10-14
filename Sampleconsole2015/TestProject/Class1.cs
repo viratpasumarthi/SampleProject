ed2bd94b0ce8f680d9b586094ac19e8f00eca0f4
viratpasumarthi <vpasumarthi1@niu.edu>
2018-10-14 16:26:22 -0500

    added binary path to firefox

--- Sampleconsole2015/TestProject/Class1.cs
+++ Sampleconsole2015/TestProject/Class1.cs
@@ -9,22 +9,26 @@ using System.Threading.Tasks;
 
 namespace TestProject
 {
-    public class Class1 :Baseclass
+
+    public class Class1
     {
         //public object Driver { get; private set; }
 
-        public IWebDriver driver;
+        //  public Baseclass CA = new Baseclass();
+
         [SetUp]
         public void initialise()
         {
+
             var timespan = TimeSpan.FromMinutes(3);
             var options = new FirefoxOptions();
-            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
-           // var service = new FirefoxDriverService()
+            // CA.Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            //driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            // var service = new FirefoxDriverService()
             //driver = new IWebDriver();
-          //  driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
+            //  driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
             //new FirefoxDriver(FirefoxBinary,FirefoxProfile,timespan);
-            //driver.Manage().Window.Maximize();
+            
 
 
             //var driver = new FirefoxDriver(binary, profile, timeSpan);
@@ -33,8 +37,17 @@ namespace TestProject
         [Test]
         public void OpenAppTets()
         {
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            //= @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            IWebDriver driver = new FirefoxDriver(service);
+            driver.Manage().Window.Maximize();
             // gecko
-            Driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
+            driver.Close();
+            // CA.Driver.Navigate().GoToUrl("http://www.google.com");
+            //driver.Navigate().GoToUrl("http://www.google.com");
 
             //driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
             // driver.
@@ -43,7 +56,8 @@ namespace TestProject
         [TearDown]
         public void EndTest()
         {
-            Driver.Close();
+            //CA.Driver.Close();
+            
         }
     }
 }
