d3988cbe5376dc199fe308058583cc25973422d3
viratpasumarthi <vpasumarthi1@niu.edu>
2018-10-14 17:10:09 -0500

    Added Web Application project

--- Sampleconsole2015/Sampleconsole2015/Program.cs
+++ Sampleconsole2015/Sampleconsole2015/Program.cs
@@ -11,14 +11,10 @@ namespace Sampleconsole2015
         public int a = 10, b = 100, c = 0;
         static void Main(string[] args)
         {
-
             MyClass MC = new MyClass();
-            //MC.a = 10;
-            //MC.b = 100;
             MC.c = MC.add(10, 200);
             Console.WriteLine("My Result is : "+ MC.c.ToString());
             Console.ReadLine(); 
-
         }
     }
 }
--- Sampleconsole2015/TestProject/Class1.cs
+++ Sampleconsole2015/TestProject/Class1.cs
@@ -11,27 +11,12 @@ namespace TestProject
 {
 
     public class Class1
-    {
-        //public object Driver { get; private set; }
-
-        //  public Baseclass CA = new Baseclass();
-
-        [SetUp]
+    {   [SetUp]
         public void initialise()
         {
-
             var timespan = TimeSpan.FromMinutes(3);
             var options = new FirefoxOptions();
-            // CA.Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
-            //driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
-            // var service = new FirefoxDriverService()
-            //driver = new IWebDriver();
-            //  driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
-            //new FirefoxDriver(FirefoxBinary,FirefoxProfile,timespan);
             
-
-
-            //var driver = new FirefoxDriver(binary, profile, timeSpan);
         }
 
         [Test]
@@ -40,24 +25,30 @@ namespace TestProject
 
             FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
             service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
-            //= @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
             IWebDriver driver = new FirefoxDriver(service);
             driver.Manage().Window.Maximize();
-            // gecko
             driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
             driver.Close();
-            // CA.Driver.Navigate().GoToUrl("http://www.google.com");
-            //driver.Navigate().GoToUrl("http://www.google.com");
-
-            //driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
-            // driver.
+            
         }
 
         [TearDown]
         public void EndTest()
         {
+            //= @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            // CA.Driver.Navigate().GoToUrl("http://www.google.com");
+            //driver.Navigate().GoToUrl("http://www.google.com");
+            //driver.Navigate().GoToUrl("http://iircdev.niu.edu/2017");
+            // driver.
+            // CA.Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            //driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(3);
+            // var service = new FirefoxDriverService()
+            //driver = new IWebDriver();
+            //  driver.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes (3);
+            //new FirefoxDriver(FirefoxBinary,FirefoxProfile,timespan);
             //CA.Driver.Close();
-            
+            //public object Driver { get; private set; }
+            //  public Baseclass CA = new Baseclass();
         }
     }
 }
